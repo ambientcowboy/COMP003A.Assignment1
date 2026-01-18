0a7d65817c9200c3e053943933ea10ae872f8700
ambientcowboy <holysignals@protonmail.com>
2026-01-17 17:40:09 -0800

    Add items 20-30

--- Program.cs
+++ Program.cs
@@ -18,6 +18,16 @@ namespace Comp003A.Assignment1
             Console.WriteLine("This is a line of text");
             Console.WriteLine("This is a line of text");
             Console.WriteLine("This is a line of text");
+            Console.WriteLine("I like cats");
+            Console.WriteLine("I like music");
+            Console.WriteLine("I like cooking");
+            Console.WriteLine("I like coffee with a little bit of creamer");
+            Console.WriteLine("I would like to get better at learning code");
+            Console.WriteLine("I am unsure if I have installed everything correctly but time will tell");
+            Console.WriteLine("There are many things I enjoy");
+            Console.WriteLine("I would like to pursue web design");
+            Console.WriteLine("I enjoy the work of many different artists");
+            Console.WriteLine("This is the 30th line of text");
         }
     }
 }
